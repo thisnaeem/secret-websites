b1a834b0e0ee110ac3161612e61fdd56de3a3541
Naeem Anjum <itsnaeemanjum@gmail.com>
2024-10-31 22:19:04 +0500

    first commit

--- app/api/websites/route.ts
+++ app/api/websites/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import prisma from '@/lib/prisma';
 
-const prisma = new PrismaClient();
+export const runtime = 'nodejs' // add this line
 
 export async function GET() {
   try {
